ba53d2c3168cca81498d4afb40d16a83ac3a1545
Quinchon <quentin.dechaux@telecomnancy.net>
2018-02-06 18:03:14 -0500

    Camera et deplacement OK

---
+++ Assets/Scripts/Camera_and_movements/CameraFollow.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    // Vistesse de la camera
+    public float CameraMoveSpeed = 120.0f;
+
+    // L'objet a suivre
+    public GameObject CameraFollowObj;
+
+    // La position a suivre
+    Vector3 FollowPOS;
+
+    // Limite de l'angle de la camera
+    public float minClampAngle = 10.0f;
+    public float maxClampAngle = 20.0f;
+
+    // La sensitivité de la camera
+    public float inputSensitivity = 150.0f;
+
+    // La camera
+    public GameObject CameraObj;
+    public GameObject PlayerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+
+    public float smoothX;
+    public float smoothY;
+    public float rotX = 0.0f;
+    public float rotY = 0.0f;
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotX = rot.x;
+        rotY = rot.y;
+        // Fige le curseur 
+        Cursor.lockState = CursorLockMode.Locked;
+        // Rend le curseur invisible
+        Cursor.visible = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // On met en place la rotation du stick
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, -minClampAngle, maxClampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+
+    }
+
+    void LateUpdate ()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        // Met en place la cible à suivre
+        Transform target = CameraFollowObj.transform;
+
+        // Se déplace vers la cible
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ Assets/Scripts/Camera_and_movements/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movementSpeed = 10;
+    public float turningSpeed = 60;
+    public float rotY = 0.0f;
+    public float mouseX;
+    public float finalInputX;
+    public float inputSensitivity = 150.0f;
+
+    private Rigidbody rb;
+    private float VerticalVelocity;
+    public float jumpForce = 10.0f;
+    private bool isGrounded = true;
+
+
+
+    void Start ()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        // saut perso
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
+    }
+
+
+    void Update()
+    {
+        // translation perso
+        float horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        transform.Translate(horizontal, 0, 0);
+
+        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+
+        // rotation perso
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        mouseX = Input.GetAxis("Mouse X");
+        finalInputX = inputX + mouseX;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        Quaternion localRotation = Quaternion.Euler(0.0f, rotY, 0.0f);
+        transform.rotation = localRotation;
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Floor" && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Floor" && isGrounded == true)
+        {
+            isGrounded = false;
+        }
+    }
+
+}
