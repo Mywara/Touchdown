745619e82a9c1f3ca27d924c414d5748b278d49b
Quinchon <quentin.dechaux@telecomnancy.net>
2018-03-29 15:17:40 -0400

    ajout des prefabs d'animation stun, confu et bouteille

---
+++ Assets/Scripts/AnimationRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationRotation : MonoBehaviour
+{
+
+    public float vitesseDeRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.transform.Rotate(Vector3.up * vitesseDeRotation * Time.deltaTime);
+
+    }
+}
