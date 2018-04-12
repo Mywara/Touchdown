1c0bd85b6f7198115d32b0c96a706b0061cec2bc
Quinchon <quentin.dechaux@telecomnancy.net>
2018-04-11 20:27:55 -0400

    ajout effet visuel passif pirate

--- Assets/Scripts/Attaque/PirateComp/PiratePassive.cs
+++ Assets/Scripts/Attaque/PirateComp/PiratePassive.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PiratePassive : MonoBehaviour {
+public class PiratePassive : Photon.PUNBehaviour
+{
 
     private int hitStack = 0;
     private bool pendingCriticalHit = false;
+    public GameObject animPiratePassif;
+
+    private void Start()
+    {
+        animPiratePassif.SetActive(false);
+    }
 
     public void IncrementHitStack()
     {
@@ -16,6 +23,7 @@ public class PiratePassive : MonoBehaviour {
         {
             Debug.Log("Pending pirate critical hit!");
             pendingCriticalHit = true;
+            this.photonView.RPC("AnimPassif", PhotonTargets.All, true);
         }
     }
 
@@ -29,5 +37,18 @@ public class PiratePassive : MonoBehaviour {
         Debug.Log("Pirate hit stack reset!");
         hitStack = 0;
         pendingCriticalHit = false;
+        this.photonView.RPC("AnimPassif", PhotonTargets.All, false);
+    }
+
+    [PunRPC]
+    private void AnimPassif(bool b)
+    {
+        animPiratePassif.SetActive(b);
+    }
+
+    public void OnDisable()
+    {
+        //On reset la classe
+        CriticalHitApplied();
     }
 }
